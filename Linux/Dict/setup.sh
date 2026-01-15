700c51f203ef9351f7f4a654ad73cb5e74731295
NoneDudeXCX <nuho.hasan10@gmail.com>
2026-01-15 05:17:37 +0300

    Version 1.7.2 Compileable versions for Linux and windows

---
+++ Linux/Dict/setup.sh
@@ -0,0 +1,3 @@
+python -m venv .venv
+source .venv/bin/activate
+pip install -r "$(dirname "$0")/req.txt"
--- Linux/setup.sh
+++
@@ -1,3 +0,0 @@
-python -m venv .venv
-source .venv/bin/activate
-pip install -r "$(dirname "$0")/req.txt"
